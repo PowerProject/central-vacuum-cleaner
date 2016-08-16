f4aa8527af79ad17caa5fbe65b68be964badbbcc
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-08-16 14:13:41 +0300

    add video, replace js file

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    var wh = $(window).height();
+    var height = function(){
+        $('.bg-cover').css({'height': wh + 'px'});
+    };
+    height();
+    $(window).resize(function(){
+        height();
+    });
+});
