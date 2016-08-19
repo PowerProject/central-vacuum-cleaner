0d49b326562f295c1ea7ca086e1847d8b27ed679
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-08-19 23:42:57 +0300

    made section-2-description, in section main change position for head

--- js/common.js
+++ js/common.js
@@ -1,11 +1,25 @@
-
 $(document).ready(function(){
-    var wh = $(window).height();
-    var height = function(){
-        $('.bg-cover').css({'height': wh + 'px'});
-    };
-    height();
-    $(window).resize(function(){
-        height();
+    var settings = function(){
+            $('.section-1-main').css({'height': $(window).height() + 'px'});
+        };
+    settings();
+
+
+    $(window).resize(function() {
+        settings();
     });
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() > $(window).height()) {
+            $('.section-1-main .video-bg__video').css('position', 'absolute');
+            $('header .navbar').css('background', 'rgba(255,255,255,1');
+        }
+        else {
+            $('.section-1-main .video-bg__video').css('position', 'fixed');
+            $('header .navbar').css('background', 'rgba(255,255,255,0.7');
+        }
+
+    });
+
+
 });
