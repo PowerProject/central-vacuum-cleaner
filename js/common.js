944aa0d64e069c47c9b605a5bddff5dc61da70d1
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-08-26 00:00:03 +0300

    add if scroll - href add class active  jquery

--- js/common.js
+++ js/common.js
@@ -3,7 +3,38 @@ $(document).ready(function(){
             $('.section-1-main').css({'height': $(window).height() + 'px'});
         };
     settings();
-
+    function scrollHref(){
+        var section2 = $('#desc').offset().top,
+            section3 = $('#advant').offset().top,
+            section4 = $('#price').offset().top,
+            section5 = $('#test').offset().top,
+            section6 = $('#contact').offset().top,
+            sectionFind = $(window).scrollTop();
+        if($(window).scrollTop()  < (section2 - 10)){
+            $('li a').removeClass('active');
+            $('li a:nth(0)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section3){
+            $('li a').removeClass('active');
+            $('li a:nth(1)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section4){
+            $('li a').removeClass('active');
+            $('li a:nth(2)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section5){
+            $('li a').removeClass('active');
+            $('li a:nth(3)').addClass('active');
+        }
+        else if ($(window).scrollTop()< section6){
+            $('li a').removeClass('active');
+            $('li a:nth(4)').addClass('active');
+        }
+        if($(window).scrollTop()+$(window).height()>=$(document).height()){
+            $('li a').removeClass('active');
+            $('li a:nth(5)').addClass('active');
+        }
+    };
 
     $(window).resize(function() {
         settings();
@@ -11,13 +42,43 @@ $(document).ready(function(){
 
     $(window).scroll(function() {
         if($(window).scrollTop() > $(window).height()) {
-         /*   $('.section-1-main .video-bg__video').css('position', 'absolute');*/
             $('header .navbar').css('background', 'rgba(255,255,255,1');
         }
         else {
-            /*$('.section-1-main .video-bg__video').css('position', 'fixed');*/
             $('header .navbar').css('background', 'rgba(255,255,255,0.7');
         }
+        scrollHref();
+    /* Scroll Href
+        var section2 = $('#desc').offset().top,
+            section3 = $('#advant').offset().top,
+            section4 = $('#price').offset().top,
+            section5 = $('#test').offset().top,
+            section6 = $('#contact').offset().top,
+            sectionFind = $(window).scrollTop();
+        if($(window).scrollTop()  < section2){
+            $('li a').removeClass('active');
+            $('li a:nth(0)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section3){
+            $('li a').removeClass('active');
+            $('li a:nth(1)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section4){
+            $('li a').removeClass('active');
+            $('li a:nth(2)').addClass('active');
+        }
+        else if ($(window).scrollTop() < section5){
+            $('li a').removeClass('active');
+            $('li a:nth(3)').addClass('active');
+        }
+        else if ($(window).scrollTop()< section6){
+            $('li a').removeClass('active');
+            $('li a:nth(4)').addClass('active');
+        }
+        if($(window).scrollTop()+$(window).height()>=$(document).height()){
+            $('li a').removeClass('active');
+            $('li a:nth(5)').addClass('active');
+        }*/
     });
 
     /* Anchor scroll*/
@@ -28,8 +89,7 @@ $(document).ready(function(){
             $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
         }
         return false; // выключаем стандартное действие
+
     });
-
-
 
 });
